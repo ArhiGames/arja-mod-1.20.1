963dedd5ff6f1c9c7b5040f6631b0b3309d8e9e5
ArhiGames <arvid.hilgers+git@gmail.com>
2024-11-16 19:11:39 +0100

    Initial commit

---
+++ src/main/java/net/arvid/arja/ArjaClient.java
@@ -0,0 +1,12 @@
+package net.arvid.arja;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArjaClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
