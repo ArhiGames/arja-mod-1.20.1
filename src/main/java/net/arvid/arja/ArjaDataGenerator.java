b6d4e3e6de642a0b4ce3565dbc1f60fbd8e74303
ArhiGames <arvid.hilgers+git@gmail.com>
2024-11-16 19:15:48 +0100

    Initial commit

--- src/main/java/net/arvid/arja/ArjaDataGenerator.java
+++ src/main/java/net/arvid/arja/ArjaDataGenerator.java
@@ -3,9 +3,11 @@ package net.arvid.arja;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class ArjaDataGenerator implements DataGeneratorEntrypoint {
+public class ArjaDataGenerator implements DataGeneratorEntrypoint
+{
 	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator)
+	{
 
 	}
 }
