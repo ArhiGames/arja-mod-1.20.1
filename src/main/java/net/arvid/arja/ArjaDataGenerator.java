69a5ebd4d9f359645533e9357715de53787e138a
ArhiGames <arvid.hilgers+git@gmail.com>
2024-11-16 22:30:36 +0100

    Added basic functionality for Datagen

--- src/main/java/net/arvid/arja/ArjaDataGenerator.java
+++ src/main/java/net/arvid/arja/ArjaDataGenerator.java
@@ -1,5 +1,6 @@
 package net.arvid.arja;
 
+import net.arvid.arja.datagen.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -8,6 +9,12 @@ public class ArjaDataGenerator implements DataGeneratorEntrypoint
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator)
 	{
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 	}
 }
